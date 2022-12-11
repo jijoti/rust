0d65a23b230c3c27a9b81c1e1d97e7d2661724fb
Joshua Lund <jijoti@yahoo.com>
2022-12-11 12:58:00 -0700

    Optimize Day 6

--- AoC/2022/day6/src/main.rs
+++ AoC/2022/day6/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
@@ -11,33 +13,56 @@ fn solve_part1(contents: &String) -> i32 {
     solve(&contents, 4)
 }
 
-fn printVec(vec: &Vec<char>) {
-    for c in vec {
-        print!("{}", c);
-    }
-    println!();
-}
+// fn print_vec(vec: &Vec<char>) {
+//     for c in vec {
+//         print!("{}", c);
+//     }
+//     println!();
+// }
 
 fn solve_part2(contents: &String) -> i32 {
     solve(&contents, 14)
 }
 
-fn solve(contents: &String, len: usize) -> i32 {
-    let mut unique: Vec<char> = Vec::new();
+// O(len * contents.len())
+// fn solve_old(contents: &String, len: usize) -> i32 {
+//     let mut unique: Vec<char> = Vec::new();
+//     let mut pos = 0;
+//     for c in contents.chars() {
+//         pos += 1;
+//         for idx in (0..unique.len()).rev() {
+//             if c == unique[idx] {
+//                 unique = unique.split_off(idx + 1);
+//                 break;
+//             }
+//         }
+//         unique.push(c);
+//         // print_vec(&unique);
+//         if unique.len() == len {
+//             break;
+//         }
+//     }
+//     pos
+// }
+
+// O(contents.len())
+fn solve(contents: &String, len: i32) -> i32 {
+    let mut last_seen: HashMap<char, i32> = HashMap::new();
+    let mut current_run = 0;
     let mut pos = 0;
     for c in contents.chars() {
-        pos += 1;
-        for idx in (0..unique.len()).rev() {
-            if c == unique[idx] {
-                unique = unique.split_off(idx + 1);
-                break;
-            }
-        }
-        unique.push(c);
-        //printVec(&unique);
-        if unique.len() == len {
+        if current_run == len {
             break;
         }
+
+        let last_seen_pos = if last_seen.contains_key(&c) {
+            *last_seen.get(&c).unwrap()
+        } else {
+            -1
+        };
+        current_run = min(pos - last_seen_pos, current_run + 1);
+        last_seen.insert(c, pos);
+        pos += 1;
     }
     pos
 }
