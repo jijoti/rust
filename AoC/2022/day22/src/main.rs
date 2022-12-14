7d023f2c684c106ea2d654931bc8e9be27913990
Joshua Lund <jijoti@yahoo.com>
2022-12-14 09:11:04 -0700

    Init rest of days with template.

--- AoC/2022/day22/src/main.rs
+++ AoC/2022/day22/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents =
+        fs::read_to_string(".\\input.txt").expect("Should have been able to read the file");
+    println!("Answer to part 1: {}", solve_part1(&contents));
+    println!("Answer to part 2: {}", solve_part2(&contents));
 }
+
+fn solve_part1(contents: &String) -> i32 {
+    0
+}
+
+fn solve_part2(contents: &String) -> i32 {
+    0
+}
+
+
